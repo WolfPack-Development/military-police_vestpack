390a30854a05de5d62e3f3182c2aac93cbb16e0d
WolfPack-Development <developmentwolfpack@gmail.com>
2025-09-07 15:12:04 -0500

    added main files

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_milpolvest.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_milpolvest.meta'
+
+
+-- WolfPack Developments
+-- WPD Astra
+-- https://discord.gg/CPX7tNrMy5
